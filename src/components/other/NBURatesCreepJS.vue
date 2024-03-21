315b5713f8974d29b290c42ad79081315d8e8977
Anatolii Zorin <Zorger27@GMail.com>
2024-03-21 03:59:27 +0200

    Small changes

--- src/components/other/NBURatesCreepJS.vue
+++ src/components/other/NBURatesCreepJS.vue
@@ -15,32 +15,29 @@ interface ExchangeRate {
       contentWidth: 0,
       marqueeWidth: 0,
       animationOffset: 0,
-      isAnimating: false, // Состояние анимации
-      animationInterval: null as any, // 'null as any' для инициализации с поддержкой TypeScript
-    }
+      isAnimating: false,
+      animationFrameId: 0, // Идентификатор для requestAnimationFrame
+    };
   },
   mounted() {
     this.fetchExchangeRates().then(() => {
       this.$nextTick(() => {
         const content = this.$refs.marquee.querySelector('.content');
         if (content) {
-          // Дублируем содержимое
           const contentHTML = content.innerHTML;
           content.innerHTML += contentHTML;
-          // Пересчитываем ширину после дублирования
           this.calculateWidths();
         }
       });
     });
-    // Следим за событием прокрутки, чтобы приостанавливать и возобновлять анимацию
     window.addEventListener('scroll', this.handleScroll);
   },
   beforeUnmount() {
-    if (this.animationInterval) {
-      clearInterval(this.animationInterval);
+    if (this.animationFrameId) {
+      cancelAnimationFrame(this.animationFrameId);
     }
-    window.removeEventListener("resize", this.calculateWidths);
     window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener('resize', this.calculateWidths);
   },
   watch: {
     speed(newSpeed: number) {
@@ -52,16 +49,11 @@ interface ExchangeRate {
     async fetchExchangeRates() {
       try {
         const response = await axios.get<ExchangeRate[]>("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
-        // Оставляем только выбранные валюты и удваиваем данные для создания непрерывного эффекта
-        // this.rates = response.data
-        //   .filter(rate => ["USD", "EUR", "GBP", "AED", "TRY", "XAU", "XAG", "XPT", "XPD"].includes(rate.cc))
-        //   .concat(response.data.filter(rate => ["USD", "EUR", "GBP", "AED", "TRY", "XAU", "XAG", "XPT", "XPD"].includes(rate.cc)));
-        // // this.rates = response.data.concat(response.data);
-
+        // Оставляем только выбранные валюты
         // let filteredRates = response.data.filter(rate => ["USD", "EUR", "GBP", "AED", "TRY", "XAU", "XAG", "XPT", "XPD"].includes(rate.cc));
         // this.rates = Array(8).fill(filteredRates).flat();
-        this.rates = Array(8).fill(response.data).flat();
 
+        this.rates = Array(8).fill(response.data).flat();
         await this.$nextTick(this.calculateWidths);
         this.setupAnimationListener();
       } catch (error) {
@@ -80,13 +72,9 @@ interface ExchangeRate {
     },
     setupAnimationListener() {
       window.addEventListener("resize", this.calculateWidths);
-      this.$nextTick(this.calculateWidths);
       this.animateMarquee();
     },
     animateMarquee() {
-      if (this.animationInterval) {
-        clearInterval(this.animationInterval);
-      }
       const step = () => {
         // Проверяем, проскроллена ли полностью первая копия содержимого
         if (this.animationOffset >= this.contentWidth) {
@@ -102,40 +90,37 @@ interface ExchangeRate {
           content.style.transform = `translateX(${-this.animationOffset}px)`;
         }
         if (this.isAnimating) {
-          requestAnimationFrame(step);
+          this.animationFrameId = requestAnimationFrame(step);
         }
       };
-
-      if (!this.isAnimating) {
-        this.isAnimating = true;
-        requestAnimationFrame(step);
-      }
+      this.isAnimating = true;
+      requestAnimationFrame(step);
     },
     pauseAnimation() {
       // Приостановка анимации
       this.isAnimating = false;
-      clearInterval(this.animationInterval);
+      if (this.animationFrameId) {
+        cancelAnimationFrame(this.animationFrameId);
+      }
     },
     handleScroll() {
       const marqueeRect = this.$refs.marquee.getBoundingClientRect();
       if (marqueeRect.top < window.innerHeight && marqueeRect.bottom > 0) {
-        // Если элемент в поле зрения, и анимация не идет, запускаем анимацию
         if (!this.isAnimating) {
           this.animateMarquee();
         }
       } else {
-        // Если элемент не в поле зрения, останавливаем анимацию
         if (this.isAnimating) {
           this.pauseAnimation();
         }
       }
     },
     updateAnimationSpeed(newSpeed: number) {
-      // Проверяем, что speed действительно изменился, прежде чем обновлять анимацию
       if (this.speed !== newSpeed) {
         this.speed = newSpeed; // Обновляем скорость в состоянии компонента
-        clearInterval(this.animationInterval); // Очищаем текущий интервал
-        this.animateMarquee(); // Запускаем анимацию с новой скоростью
+        // Перезапускаем анимацию с новой скоростью
+        this.pauseAnimation();
+        this.animateMarquee();
       }
     },
   },
